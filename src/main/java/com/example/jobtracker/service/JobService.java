3c4282824ea75422d04cce8fdf173b76ab5b2ba4
Weihang Hu <huweiha1@msu.edu>
2025-12-18 05:09:30 -0500

    Service layer Updated

--- src/main/java/com/example/jobtracker/service/JobService.java
+++ src/main/java/com/example/jobtracker/service/JobService.java
@@ -1,11 +1,16 @@
 package com.example.jobtracker.service;
 
-import com.example.jobtracker.JobtrackerApplication;
+import com.example.jobtracker.entity.JobApplication;
 import java.util.List;
 
+// Service Interface
 public interface JobService {
+    // Get a list of All jobs
+    List<JobApplication> getAllJobs();
 
-    Job createJob(Job job);
-    List<Job> getAllJobs();
-    Job getJobById(Long id);
+    // Create a job application
+    JobApplication createJob(JobApplication JobApplication);
+
+    // Get job by ID
+    JobApplication getJobById(Long id);
 }
--- src/main/java/com/example/jobtracker/service/JobServiceImpl.java
+++ src/main/java/com/example/jobtracker/service/JobServiceImpl.java
@@ -1,4 +1,34 @@
 package com.example.jobtracker.service;
 
-public class JobServiceImpl {
+import com.example.jobtracker.entity.JobApplication;
+import com.example.jobtracker.repository.JobApplicationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService{
+
+    private final JobApplicationRepository repository;
+
+    public JobServiceImpl(JobApplicationRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<JobApplication> getAllJobs() {
+        return repository.findAll();
+    }
+
+    @Override
+    public JobApplication createJob(JobApplication jobApplication) {
+        return repository.save(jobApplication);
+    }
+
+    @Override
+    public JobApplication getJobById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Job not found with id: " + id));
+    }
+
 }
